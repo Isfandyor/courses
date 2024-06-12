d91cfb31c95fedc3ff9bfe23c1609f1680e8316e
Isfandyor <universe.r136a1@gmail.com>
2024-06-12 21:15:06 +0500

    update theme

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -4,9 +4,9 @@ ThemeData lightMode = ThemeData(
   brightness: Brightness.light,
   textTheme: const TextTheme(),
   colorScheme: ColorScheme.light(
-    surface: Colors.green.shade400,
-    primary: Colors.green.shade300,
-    secondary: Colors.green.shade200,
+    surface: Colors.blue.shade900,
+    primary: Colors.blue.shade400,
+    secondary: Colors.blue.shade200,
   ),
 );
 
@@ -15,7 +15,7 @@ ThemeData darkMode = ThemeData(
   textTheme: const TextTheme(),
   colorScheme: ColorScheme.dark(
     surface: Colors.grey.shade900,
-    primary: Colors.grey.shade800,
+    primary: Colors.blue.shade800,
     secondary: Colors.grey.shade700,
   ),
 );
